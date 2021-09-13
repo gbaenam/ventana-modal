2403b5e46ab2c2a20504270be6967486aae18898
German Baena <germanb66@gmail.com>
2021-09-13 17:23:35 -0500

    Listo el modal

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+const   button  = document.getElementById('button'),
+        modal = document.getElementById('modal')
+
+
+button.addEventListener('click', () => modal.classList.add('modal--show'))
+
+
+modal.addEventListener('click', (e) => {
+    if (e.target.classList.contains('modal'))
+        modal.classList.remove('modal--show')
+})
